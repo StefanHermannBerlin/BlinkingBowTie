c4aff6c7bde6e5e4d46ac4eb801ff62c5d37b786
stefanhermann79@gmail.com <stefanhermann79@gmail.com>
2014-12-12 17:10:24 +0100

    Slightly changed to a rainbow animation.

---
+++ blinkingBowTie/blinkingBowTie.ino
@@ -0,0 +1,103 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 0
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(23, PIN);
+
+uint8_t  mode   = 2, // Current animation effect
+offset = 0; // Position of spinny eyes
+uint32_t color  = 0xffae00; // Start red
+uint32_t prevTime;
+
+void setup() {
+  pixels.begin();
+  pixels.setBrightness(60); // 1/3 brightness
+  prevTime = millis();
+}
+
+void loop() {
+  uint8_t  i;
+  uint32_t t;
+
+  switch(mode) {
+  case 0: // Random sparks - just one LED on at a time!
+    i = random(32);
+    pixels.setPixelColor(i, color);
+    pixels.show();
+    delay(10);
+    pixels.setPixelColor(i, 0);
+    break;
+
+  case 1: // Spinny wheels (8 LEDs on at a time)
+    for(i=0; i<16; i++) {
+      uint32_t c = 0;
+      if(((offset + i) & 7) < 2) c = color; // 4 pixels on...
+      pixels.setPixelColor(   i, c); // First eye
+      pixels.setPixelColor(31-i, c); // Second eye (flipped)
+    }
+    pixels.show();
+    offset++;
+    delay(100);
+    break;
+
+  case 2:
+    // Slightly different, this makes the rainbow equally distributed throughout
+    uint8_t wait = 1;
+    uint16_t i, j;
+
+    for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+      for(i=0; i< pixels.numPixels(); i++) {
+        pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + j) & 255));
+      }
+      pixels.show();
+      delay(wait);
+
+    }
+    break;
+
+
+
+
+  }
+
+  t = millis();
+  if((t - prevTime) > 8000) {      // Every 8 seconds...
+    mode++;                        // Next mode
+    if(mode > 3) {                 // End of modes?
+      mode = 0;                    // Start modes over
+      color >>= 8;                 // Next color R->G->B
+      if(!color) color = 0xffae00; // Reset to red
+    }
+    for(i=0; i<32; i++) pixels.setPixelColor(i, 0);
+    prevTime = t;
+  }
+
+
+}
+
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+    return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } 
+  else if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } 
+  else {
+    WheelPos -= 170;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void colorWipe(uint32_t c, uint8_t wait2) {
+  for(uint16_t i=0; i<pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait2);
+  }
+}
+
+
+
+
+
